3b3f6e235fc85dee6551cfb9ca7603100214822c
sherrylqwiny <92372362+sherrylqwiny@users.noreply.github.com>
2026-07-25 16:24:31 +0300

    Use environment variable for DJANGO_SETTINGS_MODULE

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -2,6 +2,9 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.development")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    os.getenv("DJANGO_SETTINGS_MODULE", "config.settings.development"),
+)
 
 application = get_asgi_application()
