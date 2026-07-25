e89dd3a75797600bf957def79d3620df11eb5e1a
sherrylqwiny <92372362+sherrylqwiny@users.noreply.github.com>
2026-07-25 15:55:30 +0300

    Add JSONRenderer to Django REST framework settings

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -150,6 +150,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Django REST Framework
 # ------------------------------------------------------------------------------
 REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+    ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
