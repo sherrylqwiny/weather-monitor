93af12e86febc9a3939b189905186ed484d3f2b6
sherrylqwiny <92372362+sherrylqwiny@users.noreply.github.com>
2026-07-25 16:25:34 +0300

    Refactor DJANGO_SETTINGS_MODULE initialization
    Update DJANGO_SETTINGS_MODULE to use environment variable.

--- backend/config/celery.py
+++ backend/config/celery.py
@@ -2,10 +2,15 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.development")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    os.getenv("DJANGO_SETTINGS_MODULE", "config.settings.development"),
+)
 
 app = Celery("weather_project")
+
 app.config_from_object("django.conf:settings", namespace="CELERY")
+
 app.autodiscover_tasks()
 
 # Import tasks module to ensure they are registered
